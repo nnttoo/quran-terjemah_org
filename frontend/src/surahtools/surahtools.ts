01f4d2dfabfedf470db2fcc8da2bd18131b10804
Anto <aaaannnnttttoooo@gmail.com>
2023-12-10 10:37:25 +0800

    use blob as webworker

--- frontend/src/surahtools/surahtools.ts
+++ frontend/src/surahtools/surahtools.ts
@@ -12,7 +12,29 @@ async function textFetch(path : string){
     return response.text();
 }
 
-export async function getAllSurah(){
+function contohFun(arg : string){
+    var fun = ()=>{
+        console.log("efefef0")
+    }
+    console.log("ini contoh ya");
+}
+
+export async function getAllSurah(){ 
+
+    var blob = new Blob(
+        [
+            contohFun.toString(),
+
+            ";"+contohFun.name + "();"
+        
+        ]
+        ,{ type: 'application/javascript' });
+
+    var url = URL.createObjectURL(blob);
+
+    var r = await (await fetch(url)).text();
+    console.log(r);
+    alert(r);
 
     let appConfig = AppContext.current.appConfig.appConfigData;
     let workerResult = await workerRunner<WorkerGetAllSurahArg,SurahData[]>({
